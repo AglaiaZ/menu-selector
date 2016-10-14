ef6ad592dfab1d830c29161102a5a56bda7d162b
ZhangBoxuan <821836281@qq.com>
2016-10-14 16:42:48 +0800

    完善功能，添加外部可控级别 完善样式

--- src/menu-selector.controller.js
+++ src/menu-selector.controller.js
@@ -9,6 +9,8 @@ MenuSelector.$inject = ['$scope', '$timeout'];
 
 function MenuSelector($scope, $timeout) {
     var vm = this;
+    var level1ScrollTop = 0;
+    var level2ScrollTop = 0;
 
     vm.selectedMenu = [];
     vm.isShow = false;
@@ -18,13 +20,13 @@ function MenuSelector($scope, $timeout) {
     vm.hoverSelectMenu = hoverSelectMenu;
 
     function selectMenu(menuList1, menuList2, menuList3) {
-        if(menuList3 && menuList2) {
+        if(menuList3 && menuList2 && vm.level === 3) {
             vm.selectedMenu = [menuList1, menuList2, menuList3];
             vm.isShow = false;
-        } else if(!menuList3 && menuList2 && menuList2.children.length === 0) {
+        } else if((!menuList3 && menuList2 && menuList2.children.length === 0) || (vm.level === 2 && menuList2)) {
             vm.selectedMenu = [menuList1, menuList2];
             vm.isShow = false;
-        } else if(!menuList3 && !menuList2 && menuList1.children.length === 0) {
+        } else if((!menuList3 && !menuList2 && menuList1.children.length === 0) || vm.level === 1) {
             vm.selectedMenu = [menuList1];
             vm.isShow = false;
         }
@@ -33,45 +35,42 @@ function MenuSelector($scope, $timeout) {
 
     function showSelectedMenu() {
         vm.isShow = !vm.isShow;
-        vm.menuList1 = vm.selectedMenu[0];
-        vm.menuList2 = vm.selectedMenu[1];
-        vm.menuList3 = vm.selectedMenu[2];
+        if(vm.selectedMenu) {
+            vm.menuList1 = vm.selectedMenu[0];
+            vm.menuList2 = vm.selectedMenu[1];
+            vm.menuList3 = vm.selectedMenu[2];
+        }
     }
 
     function hoverSelectMenu(item, $event) {
-        var timer;
-        clearTimeout(timer);
-        timer = $timeout(function() {
-            switch(item.level) {
-                case 1:
-                    vm.menuList1 = item;
-                    break;
-                case 2:
-                    vm.menuList2 = item;
-                    break;
-                case 3:
-                    vm.menuList3 = item;
-                    break;
-            }
-        }, 0);
-        $event.stopPropagation();
-
         /*
          * position
          */
+        var currentTargetTop = $event.currentTarget.offsetTop;
+        var level1 = angular.element('.level-1');
+        var level2 = angular.element('.level-2');
+        var level3 = angular.element('.level-3');
+
+        level1.find('ul').scroll(function(){
+            level1ScrollTop = level1.find('ul').scrollTop();
+        });
+        level2.find('ul').scroll(function(){
+            level2ScrollTop = level2.find('ul').scrollTop();
+        });
+
         var className = $event.currentTarget.parentNode.className;
         if(new RegExp('level-1').test(className)) {
             $timeout(function() {
-                $('.level-2').css({
-                    'margin-top': $event.currentTarget.offsetTop
-                });
+                if(level2.length !== 0) {
+                    level2[0].style.marginTop = currentTargetTop - level1ScrollTop - 6 + 'px';
+                }
             });
         }
         if(new RegExp('level-2').test(className)) {
             $timeout(function() {
-                $('.level-3').css({
-                    'margin-top': $event.currentTarget.offsetTop
-                });
+                if(level3.length !== 0) {
+                    level3[0].style.marginTop = currentTargetTop - level2ScrollTop - 6 + 'px';
+                }
             });
         }
     }
